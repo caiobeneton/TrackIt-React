76a67731a7e55e516140f6a308fdab6a66cb3200
Caio Beneton <caio.beneton@gmail.com>
2022-10-24 11:47:45 -0300

    Feat: excluir habito

--- src/Componentes/Habitos/Habitos.js
+++ src/Componentes/Habitos/Habitos.js
@@ -1,19 +1,36 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components"
 import Footer from "../Footer";
 import Header from "../Header";
 import { userContext } from "../../App";
 import axios from "axios";
+import { ThreeDots } from "react-loader-spinner"
 
-export default function Habitos(){
+export default function Habitos() {
 
     const dias = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
     const [select, setSelect] = useState([])
     const [habito, setHabito] = useState("")
     const [exibe, setExibe] = useState(false)
-    const {token} = useContext(userContext)
+    const { token } = useContext(userContext)
+    const [loading, setLoading] = useState(false)
+    const [lista, setLista] = useState([])
+    const [control, setControl] = useState(false)
 
-    function adiciona(idx){
+    useEffect(() => {
+        const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits'
+
+        const promise = axios.get(URL, { headers: { Authorization: `Bearer ${token}` } })
+        promise.then(resposta => {
+            console.log(resposta.data)
+            setLista(resposta.data)
+        })
+        promise.catch(err => {
+            console.log(err.response.data)
+        })
+    }, [token, control])
+
+    function adiciona(idx) {
         if (select.includes(idx)) {
             const novo = select.filter((elem) => elem !== idx)
             setSelect(novo)
@@ -22,7 +39,8 @@ export default function Habitos(){
         }
     }
 
-    function salvarHabito(){
+    function salvarHabito() {
+        setLoading(true)
         const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits'
         const novoHabito = {
             name: habito,
@@ -36,33 +54,68 @@ export default function Habitos(){
         const promise = axios.post(URL, novoHabito, config)
         promise.then(resposta => {
             console.log(resposta.data)
+            setLoading(false)
+            setHabito('')
+            setSelect([])
+            setExibe(false)
+            setControl(!control)
         })
         promise.catch(err => {
             console.log(err.response.data)
+            setLoading(false)
         })
     }
 
-    
-    return(
+    function excluir(id) {
+        const confirma = window.confirm('Você quer mesmo excluir esse hábito?')
+        if (confirma === true) {
+            const URL = `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}`
+            const promise = axios.delete(URL, { headers: { Authorization: `Bearer ${token}` } })
+            promise.then(() => {
+                console.log('excluiu')
+                setControl(!control)
+            })
+            promise.catch(err => {
+                console.log(err.response.data)
+            })
+        } else {
+            return
+        }
+    }
+
+
+    return (
         <>
             <StyledBody>
                 <Header></Header>
-                <StlContent>
+                <StlContent lista={lista.length > 0 ? 'none' : 'inherit'}>
                     <Topo>
                         <h1>Meus hábitos</h1>
                         <Add onClick={() => setExibe(!exibe)}>+</Add>
                     </Topo>
                     <Card disp={exibe}>
-                        <input type="text" placeholder="nome do hábito" value={habito} onChange={e => setHabito(e.target.value)} ></input>
+                        <input type="text" placeholder="nome do hábito" value={habito} onChange={e => setHabito(e.target.value)} disabled={loading} ></input>
                         <Semana>
-                            {dias.map((d, idx) => <Dias cor={select.includes(idx)} onClick={() => adiciona(idx)} key={idx} >{d}</Dias>)}
+                            {dias.map((d, idx) => <Dias disabled={loading} cor={select.includes(idx)} onClick={() => adiciona(idx)} key={idx} >{d}</Dias>)}
                         </Semana>
                         <Botoes>
-                            <Cancelar>Cancelar</Cancelar>
-                            <Salvar onClick={salvarHabito}>Salvar</Salvar>
+                            <Cancelar onClick={() => setExibe(!exibe)}>Cancelar</Cancelar>
+                            <Salvar loading={loading ? '0.7' : '1'} onClick={salvarHabito}>
+                                {loading ? <ThreeDots width="51" height='13' color='#FFFFFF' visible={true} /> : 'Salvar'}
+                            </Salvar>
                         </Botoes>
                     </Card>
                     <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+                    {lista.map((h) => <Habito key={h.id}>
+                        <Topo>
+                            <h1>{h.name}</h1>
+                            <ion-icon onClick={() => excluir(h.id)} name="trash-outline"></ion-icon>
+                        </Topo>
+
+                        <Semana>
+                            {dias.map((d, idx) => <Dias key={idx} cor={h.days.includes(idx)}>{d}</Dias>)}
+                        </Semana>
+                    </Habito>)}
                 </StlContent>
                 <Footer></Footer>
             </StyledBody>
@@ -94,11 +147,17 @@ const StlContent = styled.div`
         font-weight: 400;
         font-size: 17.976px;
         color: #666666;
+        display: ${props => props.lista};
     }
 `
 const Topo = styled.div`
     display: flex;
     justify-content: space-between;
+    box-sizing: border-box;
+    padding-right: 10px;
+    ion-icon {
+        font-size: 22px;
+    }
 `
 const Add = styled.div`
     width: 40px;
@@ -184,4 +243,21 @@ const Salvar = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    opacity: ${props => props.loading};
+`
+const Habito = styled.div`
+    width: 340px;
+    height: 91px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    padding-left: 15px;
+    padding-top: 10px;
+    box-sizing: border-box;
+    margin-top: 10px;
+    h1{
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 20px;
+        font-weight: 400;
+        color: #666666;
+    }
 `
--- src/Componentes/Hoje/Hoje.js
+++ src/Componentes/Hoje/Hoje.js
@@ -7,7 +7,11 @@ export default function Hoje(){
         <>
             <StyledBody>
                 <Header></Header>
-                <h1>Hoje</h1>
+                <StyledContent>
+                    <Topo>
+                        <h1>Hoje</h1>
+                    </Topo>
+                </StyledContent>
                 <Footer></Footer>
             </StyledBody>
         </>
@@ -19,4 +23,23 @@ const StyledBody = styled.div`
     height: 530px;
     width: 100%;
     margin-top: 70px;
+`
+const StyledContent = styled.div`
+    margin-left: 17px;
+    padding-top: 30px;
+    box-sizing: border-box;
+    h1 {
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22.976px;
+        color: #126BA5;
+        margin-bottom: 17px;
+    }
+`
+const Topo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding-right: 10px;
 `
